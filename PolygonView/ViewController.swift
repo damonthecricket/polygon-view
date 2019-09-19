c7dce463bfeadf9681cd4e18e02e302db7362da6
Damon Cricket <d.a.tsvirko@gmail.com>
2019-09-19 10:30:00 +0200

    First commit.

---
+++ PolygonView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PolygonView
+//
+//  Created by Damon Cricket on 19.09.2019.
+//  Copyright © 2019 DC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
